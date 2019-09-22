643ae1bae4f18bea31ee067c5c71056ff4bbbcc6
lishu <lishu14@foxmail.com>
2019-09-22 21:31:03 +0800

    lishu

--- 1.two-sum.cpp
+++ 1.two-sum.cpp
@@ -9,32 +9,11 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> method_2(vector<int>& nums, int target)
-	{
-/*
-  ✔ Your runtime beats 92.54 % of cpp submissions
-  ✔ Your memory usage beats 57.22 % of cpp submissions (9.8 MB)
-*/
-		vector<int> v(2,0);
-		// val+id
-		unordered_map<int, int> hash;
-		// we can search num and insert it to hash map at same time
-		// and current num must be not in hash map
-		for (int i = nums.size(); i--; hash[nums[i]] = i) //注意hash[nums[i]] = i是在循环结束时执行。美极了！
-		{
-			if (hash.find(target - nums[i]) == hash.end())
-				continue;
-
-			v[0] = i;           // the index from 0 to n-1
-			v[1] = hash[target - nums[i]];
-			return v;
-		}
-		return v;                   // no answer return {0,0}
-	}
-    vector<int> method_1(vector<int>& nums, int target) {
+    vector<int> twoSum1(vector<int>& nums, int target) {
 /*
   ✔ Your runtime beats 15.23 % of cpp submissions
   ✔ Your memory usage beats 93.73 % of cpp submissions (9.2 MB)
+  Written by others
 */
         vector<int> v;
         for (int i=0; i < nums.size();i++){
@@ -48,10 +27,30 @@ public:
         }
         return v;
     }
+    vector<int> twoSum2(vector<int>& nums, int target) {
+/*
+  ✔ Your runtime beats 92.54 % of cpp submissions
+  ✔ Your memory usage beats 57.22 % of cpp submissions (9.8 MB)
+  Written by others
+*/
+		vector<int> v(2,0);
+		// val+id
+		unordered_map<int, int> hash;
+		// we can search num and insert it to hash map at same time
+		// and current num must be not in hash map
+		for (int i = nums.size(); i--; hash[nums[i]] = i) //注意hash[nums[i]] = i是在循环结束时执行。美极了！
+		{
+			if (hash.find(target - nums[i]) == hash.end())
+				continue;
+			v[0] = i;           // the index from 0 to n-1
+			v[1] = hash[target - nums[i]];
+			return v;
+		}
+		return v;                   // no answer return {0,0}
+	}
     vector<int> twoSum(vector<int>& nums, int target) {
-		return method_1(nums, target);
+		return twoSum1(nums, target);
 	}
 };
-
 
 
--- 2.add-two-numbers.cpp
+++ 2.add-two-numbers.cpp
@@ -12,60 +12,49 @@
 //  * };
  */
 #include <string>
-# include <iostream>
-
+#include <iostream>
 using namespace std;
-
 // struct ListNode {
 //       int val;
 //       ListNode *next;
 //       ListNode(int x) : val(x), next(NULL) {}
 //   };
-
 class Solution {
 public:
-    ListNode *addTwoNumbers2(ListNode *l1, ListNode *l2) {
+    ListNode *addTwoNumbers1(ListNode *l1, ListNode *l2) {
 /*
-  ✔ Your runtime beats 63.61 % of cpp submissions
+  ✔ Your runtime beats 63.61 % of cpp submissions. This number varies from 20% to 90 % which is untrustable
   ✔ Your memory usage beats 45.14 % of cpp submissions (10.5 MB)
+  Written by others
 */
         int x=0, y=0, carry=0, sum=0;
         ListNode *h=NULL, **t=&h;
-        
         while (l1!=NULL || l2!=NULL){
             x = getValueAndMoveNext(l1);
             y = getValueAndMoveNext(l2);
-            
             sum = carry + x + y;
-            
             ListNode *node = new ListNode(sum%10);
             *t = node;
             t = (&node->next);
-            
             carry = sum/10;
         }
-        
         if (carry > 0) {
             ListNode *node = new ListNode(carry%10);
             *t = node;
         }
-        
         return h;
     };
 
-
-    ListNode *addTwoNumbers3(ListNode *l1, ListNode *l2) {
+    ListNode *addTwoNumbers2(ListNode *l1, ListNode *l2) {
 /*
-  ✔ Your runtime beats 63.61 % of cpp submissions
+  ✔ Your runtime beats 63.61 % of cpp submissions.This number varies from 20% to 90 % which is untrustable
   ✔ Your memory usage beats 92.57 % of cpp submissions (10.1 MB)
 */
         int x=0, y=0, carry=0, sum=0;
         ListNode *h=NULL, *t=NULL;
-        
         while (l1!=NULL || l2!=NULL){
             x = getValueAndMoveNext(l1);
             y = getValueAndMoveNext(l2);
-            
             sum = carry + x + y;
             if (h==NULL){
                 h = new ListNode(sum%10);
@@ -77,19 +66,16 @@ public:
             }
             carry = sum/10;
         }
-        
         if (carry > 0) {
             ListNode *node = new ListNode(carry%10);
             t->next = node;
             t = t->next;
         }
-
         return h;
     }
 
     ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
         return addTwoNumbers2(l1, l2);
-        // return s;
     }
 private:
     int getValueAndMoveNext(ListNode* &l){
@@ -100,7 +86,6 @@ private:
         }
         return x;
     }
-
 };
 
 
