c246de2d96918cd076a9928ea6fc17b51a8f00ca
lishu <lishu14@foxmail.com>
2019-12-17 01:21:45 +0800

    li

--- 704.binary-search.cpp
+++ 704.binary-search.cpp
@@ -10,6 +10,9 @@ using namespace std;
 /*
 方法一超时？？
 方法二就没问题···
+方法三数组越界了似乎。
+
+记住第二种方法吧。虽然看起来笨了点。
 */
 class Solution {
 public:
@@ -27,27 +30,29 @@ public:
     int search2(vector<int>& nums, int target) {
         int left = 0, right = nums.size() - 1, mid;
         while (left <= right) {
-        mid = (left + right) >> 1;
-        if (nums[mid] == target) return mid;
-        else if (nums[mid] > target) right = mid - 1;
-        else left = mid + 1;
+            mid = (left + right) >> 1;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] > target) right = mid - 1;
+            else left = mid + 1;
         }
         return -1;
     }   
     
+    int search3(vector<int> & a, int target) {
+        int l=0,r=a.size(),mid;
+        while(l<=r) {
+            mid = (l+r) >> 1;
+            if (target == a[mid]) return mid;
+            else if (target < a[mid]) r = mid-1;
+            else  l = mid+1;
+        }
+        return -1;
+    }
+
+
     int search(vector<int>& nums, int target) {
         return search2(nums, target);
     }
 };
 // @lc code=end
 
-int search(vector<int> & a, int target) {
-    int l=0,r=a.size(),mid;
-    while(l<=r) {
-        mid = (l+r) >> 1;
-        if (target == a[mid]) return mid;
-        else if (target < a[mid]) r = mid-1;
-        else  l = mid+1;
-    }
-    return -1;
-}
